56a893b542940f81742e54e4876bca287095ecb7
Gustavo Aguilar <gangeagui@gmail.com>
2026-01-07 02:10:45 -0600

    feat: day 001 - java variables fundamentals

---
+++ docs/learning-log.md
@@ -0,0 +1,11 @@
+# Learning Log
+
+This document tracks my daily progress during the 365 Days of Code challenge.
+
+---
+
+## Day 001 - Variables
+
+- Practiced basic variable declaration and initialization in Java
+- Used primitive data types and String
+- Followed clean package and class naming conventions
---
+++ src/fundamentals/variables/Day001Variables.java
@@ -0,0 +1,36 @@
+package fundamentals.variables;
+
+/**
+ * Day 001 - Variables
+ *
+ * Objective:
+ * - Understand how to declare and initialize variables in Java
+ * - Learn basic primitive data types
+ */
+
+public class Day001Variables {
+
+    public static void main(String[] args) {
+
+        // Integer type
+        int age = 23;
+
+        // Floating point type
+        double salary = 1500.50;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // String (reference type)
+        String name = "Gustavo";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun: " + isJavaFun);
+    }
+}
